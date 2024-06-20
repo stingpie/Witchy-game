2e9a6468e277947f84af8febc09164b4feb92a25
stingpie <stingpie52@gmail.com>
2024-06-20 10:26:36 -0500

    added projectile scene

--- pointer.gd
+++ pointer.gd
@@ -2,20 +2,23 @@ extends Node3D
 
 
 var cam
+var player_body
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	cam = $"../Camera3D"
-	print(cam)
+	player_body = $".."
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	var dropPlane  = Plane(Vector3(0, 1, 0), 0)
+	var dropPlane  = Plane(Vector3(0, 1, 0), -player_body.position.y/2)
 	var position3D = dropPlane.intersects_ray(
 							 cam.project_ray_origin(get_viewport().get_mouse_position()),
 							 cam.project_ray_normal(get_viewport().get_mouse_position()))
-	rotation.y = 3.141592 / 2 -Vector2(position3D.x, position3D.z).angle_to_point(Vector2($"..".position.x,$"..".position.z))
+	if(position3D):
+		rotation.y = 3.141592 / 2 -Vector2(position3D.x, position3D.z).angle_to_point(Vector2(player_body.position.x,player_body.position.z))
 	
 	
 	pass
