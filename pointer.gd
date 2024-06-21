388b0fe7924f9f3e17023329e453d1d461d0aa11
Luis <luisga1337@gmail.com>
2024-06-21 13:03:29 -0500

    Added herb.tscn and modified pointer.gd to shoot from right mouse click and slower

--- pointer.gd
+++ pointer.gd
@@ -64,4 +64,27 @@ func _process(delta):
 		
 		# add the projectile to the map the player is currently in
 		projectile_map.add_child(projectile)
+	
+	if Input.is_action_pressed("right click") and position3D and cool_down <=0: # if the position is valid, and the left mouse button in pressed,
+		
+		cool_down = default_reload * 2; ## TODO: make this change with modifiers
+		
+		var projectile = projectile_scene.instantiate() # instantiate a preloaded scene (initialize it)
+		projectile.made_by=$".."
+		# calculate the direction the pointer is pointing in
+		var direction = (Vector3(position3D.x, player_body.position.y, position3D.z) - player_body.position).normalized()
+		# set the velocity of the projectile to the player's vel, plus the direction times speed. 
+		# (Ie, make the projectile move relative to the player)
+		projectile.velocity = player_body.velocity + direction * proj_speed
+		
+		# set the position of the particle at the end of the player's pointer.
+		projectile.position.x = player_body.position.x + direction.x * 1.15
+		projectile.position.y = -player_body.position.y/2 + direction.y * 1.15 # for some reason, player.pos.y is messed up. you have to divide by -2 to get the correct result.
+		projectile.position.z = player_body.position.z + direction.z * 1.15
+		
+		projectile.wand_modifiers = wand_modifiers # apply set modifers to projectile.
+		
+		# add the projectile to the map the player is currently in
+		projectile_map.add_child(projectile)
+		
 	pass
