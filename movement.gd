4827bd1d53c737b1fb16ad203a04ba34050c7adf
stingpie <stingpie52@gmail.com>
2024-06-21 10:16:37 -0500

    improvement

--- movement.gd
+++ movement.gd
@@ -3,17 +3,17 @@ extends CharacterBody3D
 
 
 const thresh=0.1
-const SPEED = 10.00
+const SPEED = 9.00
 const JUMP_VELOCITY = 4.5
 
-const max_speed = 20.0
+const max_speed = 15.0
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var mass = 15.0
-var COF = 3.0
+var mass = 30.0
+var COF = 3.5
 
 var state ="idle" # potential states : idle, running, slipping, slow running. 
 
@@ -40,8 +40,10 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Vector2(Input.get_axis("ui_left","ui_right"), Input.get_axis("ui_up", "ui_down")) 
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))#.normalized()
+
+	
 	
 	if(direction):
 		state = "running"
@@ -54,10 +56,12 @@ func _physics_process(delta):
 		# you can reverse easily, but not turn quickly. I might change this. Depends on how it
 		# plays.
 		
-		if(velocity.length() < max_speed):
-			force.x = direction.x * SPEED if state == "running" else SPEED/2
-			force.z = direction.z * SPEED if state == "running" else SPEED/2
+		#if(velocity.length() < max_speed):
+		force.x = mass/15 * direction.x * SPEED if state == "running" else SPEED/2
+		force.z = mass/15 * direction.z * SPEED if state == "running" else SPEED/2
 			# * (2-thresh)*( 1 + max(0, thresh-velocity.dot(direction)))
+			
+		#force *=  max(0,min(1,  (max_speed/(abs(velocity.length())+0.001)) - (velocity.normalized() - direction.normalized()).length()    ))
 			
 		_animated_sprite.play("running")
 		
@@ -75,6 +79,12 @@ func _physics_process(delta):
 	# set acceleration due to the force
 	acceleration.x = force.x / mass
 	acceleration.z = force.z / mass
+	
+	if(velocity.length()<0.25):
+		velocity *= 0.9;
+		acceleration *= 0.9;
+		force *= 0.9;
+	
 	
 	if(is_on_floor()):
 		# apply acceleration if the player is on the floor (ie, not jumping) 
