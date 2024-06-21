c48a1c039a3d727ee9485b455c880d429cca9778
stingpie <stingpie52@gmail.com>
2024-06-21 09:43:59 -0500

    added proj lifetime and changed movement

--- movement.gd
+++ movement.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 
 const thresh=0.1
-const SPEED = 6.00
+const SPEED = 10.00
 const JUMP_VELOCITY = 4.5
 
 const max_speed = 20.0
@@ -15,7 +15,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mass = 15.0
 var COF = 3.0
 
-var state ="idle"
+var state ="idle" # potential states : idle, running, slipping, slow running. 
 
 var force = Vector3(0,0,0)
 var acceleration = Vector3(0,0,0)
@@ -43,27 +43,30 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	if(direction):
+		state = "running"
+	else:
+		state = "idle"
 	
-	
-	if direction:
+	if direction and (state == "running" or state == "slow running"):
 		# apply a force to the character. 
 		# Here, the force is proportional to the change in angle between the new and old direction.
 		# you can reverse easily, but not turn quickly. I might change this. Depends on how it
 		# plays.
 		
 		if(velocity.length() < max_speed):
-			force.x = direction.x * SPEED * (2-thresh)*( 1 + max(0, thresh-velocity.dot(direction)))
-			force.z = direction.z * SPEED * (2-thresh)*( 1 + max(0, thresh-velocity.dot(direction)))
+			force.x = direction.x * SPEED if state == "running" else SPEED/2
+			force.z = direction.z * SPEED if state == "running" else SPEED/2
+			# * (2-thresh)*( 1 + max(0, thresh-velocity.dot(direction)))
 			
 		_animated_sprite.play("running")
-		state = "running"
+		
 			
 	else:
 		force.x = 0
 		force.z = 0
 		
 		_animated_sprite.play("idle")
-		state = "idle"
 	
 	# apply friction
 	force.x = force.x - sign(velocity.x) * friction_force * delta
--- projectile.gd
+++ projectile.gd
@@ -6,9 +6,16 @@ func _hit_building(area): #area is the Area2D of the building
 var damage =10
 var made_by;
 
+const lifespan=10;
+var time_left = lifespan;
+
 var wand_modifiers=[]
 
 func _physics_process(delta):
+	time_left = max(0, time_left - delta)
+	
+	if(time_left<=0):
+		queue_free()
 	
 	for modifier in wand_modifiers:
 		if modifier=="decelerate":
@@ -20,4 +27,5 @@ func _on_area_3d_body_entered(body):
 	
 	if body is CharacterBody3D and body != made_by :
 		body.damage(damage)
+		queue_free()
 	pass # Replace with function body.
