6884eeb8ad1bc29d133c6d44852dd3b375525eda
stingpie <stingpie52@gmail.com>
2024-06-24 20:26:58 -0500

    added some scaffolding for dialog

---
+++ npc.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+
+
+@export_file var file
+
+var dialog
+
+func _ready():
+	
+	var f = FileAccess.open(file, FileAccess.READ)
+	var json_string = f.get_as_text()
+	var json = JSON.new()
+	var error = json.parse(json_string)
+	if error == OK:
+		var data_received = json.data
+		if typeof(data_received) == TYPE_ARRAY:
+			print(data_received) # Prints array
+		else:
+			print("Unexpected data")
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+	f.close()
+
+
+func _physics_process(delta):
+	
+	if $"../Player/PlayerBody3D" in $"Area3D".get_overlapping_bodies():
+		if Input.is_action_just_pressed("interact"):
+			print("PUT DIALOG HANDLING HERE ...")
+	
