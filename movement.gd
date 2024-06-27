21dfb75e15ec92aa24a54ed603d3ca8cca0b2e56
stingpie <stingpie52@gmail.com>
2024-06-27 10:04:49 -0500

    fixed ui scaling. (it happens now)

--- movement.gd
+++ movement.gd
@@ -156,7 +156,7 @@ func _physics_process(delta):
 			time_effects.erase(event)
 	
 	if(Input.is_action_just_pressed("time spell")):
-		HP -= 10
+		damage(10)
 		var event = TimeEvent.new()
 		event.type = "HP"
 		event.variable=10
